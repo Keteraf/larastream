94fe999cc888efba31d4df166592203396be8686
Raffaele Longo Elia <raffaele.longo.elia@gmail.com>
2023-07-18 13:46:44 +0200

    Added Laravel IDE Helper

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
